1cc39075ce86b0c2c6bc8338ac643c450e23d172
ibrusko <ibrusko@gear-games.com>
2025-01-22 16:24:04 +0700

    Garage controller

--- Assets/Scripts/Installers/GameSceneInstaller.cs
+++ Assets/Scripts/Installers/GameSceneInstaller.cs
@@ -1,9 +1,17 @@
+using UnityEngine;
 using Zenject;
 
 public class GameSceneInstaller : MonoInstaller
 {
+    [SerializeField] private GarageController _garageController;
+    
     public override void InstallBindings()
     {
-        
+        Container.BindInterfacesTo<GarageController>().FromInstance(_garageController).AsSingle();
+        //Container.Bind<HandController>().AsSingle();
+        //Container.Bind<MovementController>().AsSingle();
+        //Container.Bind<ItemsSpawner>().AsSingle();
+        //Container.Bind<TargetController>().AsSingle();
+        //Container.Bind<InputController>();
     }
 }
